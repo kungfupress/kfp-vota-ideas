058dcf2cd7af1a288117b580d3fd928334ecdc42
Tonio Ruiz <kungfupress@gmail.com>
2019-09-05 17:02:46 +0200

    Modifica la carga de javascript y comienza el recuento de votos

--- include/idea-list.php
+++ include/idea-list.php
@@ -8,63 +8,25 @@ defined( 'ABSPATH' ) or die();
 
 // Crea el shortcode para mostrar el listado de ideas
 add_shortcode('kfp_vti_idea_list', 'Kfp_Vti_Idea_list');
-
-// Prepara los hooks para grabar con ajax
-add_action('wp_ajax_vti_idea_vote', 'Kfp_Vti_Idea_vote');
-add_action('wp_ajax_nopriv_vti_idea_vote', 'Kfp_Vti_Idea_vote');
-function Kfp_Vti_Idea_vote() {
-    if ( defined('DOING_AJAX') && DOING_AJAX 
-        && wp_verify_nonce($_POST['nonce'], 'link_click_vote_' . admin_url( 'admin-ajax.php'))) {
-        // Crea un CPT de tipo Voto con la IP del visitante actual
-        $args = array(
-            'post_title' => 'kfp-vti-title',
-            'post_content' => $_SERVER['REMOTE_ADDR'],
-            'post_type' => 'vti_vote',
-            'post_status' => 'publish',
-            'post_parent' => filter_input(INPUT_POST, 'post_id', FILTER_SANITIZE_NUMBER_INT),
-            'comment_status' => 'closed',
-            'ping_status' => 'closed'
-        );
-    
-        // Esta variable $post_id contiene el ID del nuevo registro 
-        // Nos vendría de perlas para grabar los metadatas
-        $post_id = wp_insert_post($args);
-        // Devuelve un token para saber que se ha grabado el voto
-        echo "pass";
-        die();
-    } else {
-        die('Error de seguridad');
-    }
-}
-
-// carga y 'localiza' el script ¿para introducir el nonce?
-add_action( 'wp_enqueue_scripts', 'ajax_vote_link_enqueue_scripts' );
-function ajax_vote_link_enqueue_scripts() 
-{
-    wp_enqueue_script('vote-link', plugins_url('../js/vote-link.js', __FILE__));
-    wp_localize_script('vote-link', 'votelink', array(
-        'ajax_url' => admin_url('admin-ajax.php'),
-        'ajax_nonce' => wp_create_nonce('link_click_vote_' . admin_url('admin-ajax.php')),
-    ));
-}
-
-/**
- * Muestra en el frontend una lista de las ideas publicadas
- * Desde esta lista es desde donde se pueden votar las ideas
- *
- * @return void
- */
 function Kfp_Vti_Idea_list()
 {
+    wp_enqueue_script('kfp-vti-vote-link');
     $args = array('post_type' => 'vti_idea', 'posts_per_page' => 30);
     $the_query = new WP_Query( $args ); 
     if ($the_query->have_posts()) { 
         $html = '<table>';
         while ($the_query->have_posts()) { 
             $the_query->the_post();
+            $parent_id = get_the_ID();
+            $children = get_children( array(
+                'post_parent' => $parent_id,
+                'post_type'   => 'vti_vote', 
+                'numberposts' => -1,
+                'post_status' => 'any' 
+            ) );
             $html .= '<tr><td><b>'. get_the_title() . '</b>'; // Importante lo del get_
             $html .= '<br>' . get_the_content() .'</td>';
-            $html .= '<td nowrap>5 votos - <a href="#" class="click-vote-link"';
+            $html .= '<td nowrap>' . $children->found_posts . ' votos - <a href="#" class="click-vote-link"';
             $html .= 'data-idea-id="' . get_the_ID() . '">Votar</a></td></tr>';
         }
         $html .= '</table>';
@@ -73,4 +35,41 @@ function Kfp_Vti_Idea_list()
     }
     
     return $html; // No hace echo o print desde un shortcode, usar return 
+}
+
+add_action( 'wp_enqueue_scripts', 'kfp_Vti_Idea_List_scripts' );
+function Kfp_Vti_Idea_List_scripts() 
+{
+    wp_register_script('kfp-vti-vote-link', plugins_url('../js/vote-link.js', __FILE__), array('jquery'));
+    wp_localize_script('kfp-vti-vote-link', 'ajax_object', array(
+        'ajax_url' => admin_url('admin-ajax.php'),
+        'ajax_nonce' => wp_create_nonce('link_click_vote_' . admin_url('admin-ajax.php')),
+    )); 
+}
+
+// Prepara los hooks para grabar con ajax
+add_action('wp_ajax_vti_idea_vote', 'Kfp_Vti_Idea_vote');
+add_action('wp_ajax_nopriv_vti_idea_vote', 'Kfp_Vti_Idea_vote');
+function Kfp_Vti_Idea_vote() {
+    if ( defined('DOING_AJAX') && DOING_AJAX){ 
+        // && wp_verify_nonce($_POST['nonce'], 'link_click_vote_' . admin_url( 'admin-ajax.php'))) {
+        // Crea un CPT de tipo Voto con la IP del visitante actual
+        $args = array(
+            'post_title' => 'kfp-vti-title',
+            'post_content' => $_SERVER['REMOTE_ADDR'],
+            'post_type' => 'vti_vote',
+            'post_status' => 'publish',
+            'post_parent' => filter_input(INPUT_POST, 'idea_id', FILTER_SANITIZE_NUMBER_INT),
+            'comment_status' => 'closed',
+            'ping_status' => 'closed'
+        );
+        // Esta variable $vote_id contiene el ID del nuevo registro 
+        // Nos vendría de perlas para grabar los metadatas
+        $vote_id = wp_insert_post($args);
+        // Devuelve un token para saber que se ha grabado el voto
+        echo "vota";
+        die();
+    } else {
+        die('Error de seguridad');
+    }
 }
--- js/vote-link.js
+++ js/vote-link.js
@@ -1,12 +1,14 @@
+/*
 jQuery( document ).on( 'click', '.click-vote-link', function(event) {
     event.preventDefault();
-    var post_id = jQuery(this).data('vote-id');
+    var idea_id = jQuery(this).data('idea-id');
     jQuery.ajax({
         type : 'post',
         data : {
-            action : 'click_vote_process',
-            nonce : votelink.ajax_nonce,
-            post_id : post_id
+            url: 'http://wordpress.local/wp-admin/admin-ajax.php',
+            action : 'vti_idea_vote',
+            nonce : 'pepe',
+            idea_id : idea_id
         },
         success : function( response ) {
             if (response == "pass") {
@@ -17,3 +19,24 @@ jQuery( document ).on( 'click', '.click-vote-link', function(event) {
         }
     });
 })
+*/
+
+//             url: votelink.ajax_url,
+//             nonce : votelink.ajax_nonce,
+
+
+jQuery(document).ready(function($){
+    $('.click-vote-link').click(function(event){
+        event.preventDefault();
+        var redirect = $(this).attr('href');
+        $.post(ajax_object.ajax_url, 
+            {
+                action:'vti_idea_vote', 
+                idea_id:$(this).data('idea-id')
+            }, 
+            function(response) {
+                alert(response);
+            });
+        return false;
+    });
+});
