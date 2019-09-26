3f1c11040b7aed55efe4c1fddd68dac9f0168013
Juanan Ruiz <juanan@us.es>
2019-09-26 15:33:58 +0200

    Elimina código innecesario

--- js/vote-link.js
+++ js/vote-link.js
@@ -1,41 +1,18 @@
-/*
-jQuery( document ).on( 'click', '.click-vote-link', function(event) {
-    event.preventDefault();
-    var idea_id = jQuery(this).data('idea-id');
-    jQuery.ajax({
-        type : 'post',
-        data : {
-            url: 'http://wordpress.local/wp-admin/admin-ajax.php',
-            action : 'vti_idea_vote',
-            nonce : 'pepe',
-            idea_id : idea_id
-        },
-        success : function( response ) {
-            if (response == "pass") {
-                // Incrementa el contador
-            } else {
-                alert(response);
-            }
-        }
-    });
-})
-*/
+/** js/vote-link.js */
 
-//             url: votelink.ajax_url,
-//             nonce : votelink.ajax_nonce,
-
-
-jQuery(document).ready(function($){
-    $('.click-vote-link').click(function(event){
+jQuery(document).ready(function ($) {
+    $('body').on('click', '.vote', function (event) {
         event.preventDefault();
-        var redirect = $(this).attr('href');
-        $.post(ajax_object.ajax_url, 
+        var $link = $(this);
+        var $linkCell = $link.parents('td');
+        $.post(ajax_object.ajax_url,
             {
-                action:'vti_idea_vote', 
-                idea_id:$(this).data('idea-id')
-            }, 
-            function(response) {
-                alert(response);
+                action: 'vti_idea_vote',
+                nonce: ajax_object.ajax_nonce,
+                idea_id: $link.data('idea-id')
+            },
+            function (response) {
+                $linkCell.html(response);
             });
         return false;
     });
